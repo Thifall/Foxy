9f303527490bef67f5daf1f26388bdb49ae3d4da
Jan Musioł <migosh@o2.pl>
2024-10-17 11:21:51 +0200

    [jm][+] Added player states, added debug label, player now changes animations based on state

--- Scenes/player/player.gd
+++ Scenes/player/player.gd
@@ -2,29 +2,39 @@ extends CharacterBody2D
 
 class_name Player
 
-@onready var sprite_2d: Sprite2D = $Sprite2D
+enum PlayerState { IDLE, RUN, JUMP, FALL, HURT }
 
+@onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var debug_label: Label = $DebugLabel
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 const GRAVITY: float = 690.0
 const RUN_SPEED: float = 120.0
 const MAX_FALL: float = 400.0
 const JUMP_VELOCITY: float = -260.0
 
+var _state: PlayerState = PlayerState.IDLE
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
 	if !is_on_floor():
 		velocity.y += GRAVITY * delta
 		
-	getInput()
+	get_input()
 	move_and_slide()
+	calculate_state()
+	update_debug_label()
 		
+		
+func update_debug_label() -> void:
+	debug_label.text = "On floor: %s\nVelocity: %.0f,%.0f\nstate: %s" % \
+	[ is_on_floor(), velocity.x, velocity.y, PlayerState.keys()[_state] ]
 
-func getInput() -> void:
+func get_input() -> void:
 	velocity.x = 0
 	if Input.is_action_pressed("Left"):
 		velocity.x = -RUN_SPEED
@@ -37,3 +47,33 @@ func getInput() -> void:
 		velocity.y = JUMP_VELOCITY
 	
 	velocity.y = clampf(velocity.y, JUMP_VELOCITY, MAX_FALL)
+
+func calculate_state() -> void:
+	if is_on_floor():
+		if velocity.x == 0:
+			set_state(PlayerState.IDLE)
+		else:
+			set_state(PlayerState.RUN)
+	else:
+		if velocity.y > 0:
+			set_state(PlayerState.FALL)
+		else:
+			set_state(PlayerState.JUMP)
+
+func set_state(newState: PlayerState) -> void:
+	if newState == _state:
+		return;
+	_state = newState
+	match _state:
+		PlayerState.IDLE:
+			animation_player.play("Idle")
+		PlayerState.RUN:
+			animation_player.play("Run")
+		PlayerState.JUMP:
+			animation_player.play("Jump")
+		PlayerState.FALL:
+			animation_player.play("Fall")
+		PlayerState.HURT:
+			animation_player.play("Hurt")
+	
+	
